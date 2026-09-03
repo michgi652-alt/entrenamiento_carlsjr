9e97b641e5d2f58566624066a8d4e1e05a8d30a5
Michel Garcia Iturbero <michgi652@gmail.com>
2026-09-03 16:13:12 -0600

    Primer commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def login():
+    return render_template('index.html')
+
+@app.route('/guardar', methods=['POST'])
+def guardar():
+    return redirect(url_for('inicio'))
+
+@app.route('/inicio')
+def inicio():
+    return render_template('inicio.html')
+
+@app.route('/cursos')
+def cursos():
+    return render_template('cursos.html')
+
+@app.route('/perfil')
+def perfil():
+    return render_template('perfil.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
